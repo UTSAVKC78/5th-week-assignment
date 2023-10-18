db8b2412b99c598970895ca54de3b064d88b6851
UTSAVKC78 <144709828+UTSAVKC78@users.noreply.github.com>
2023-10-18 12:33:43 +0900

    Update assignment5.3.py

--- assignment5.3.py
+++ assignment5.3.py
@@ -1,10 +1,8 @@
 input_string = input("enter a string: ")
-
 num_counting = 0
 upp_counting = 0
 low_counting = 0
 other_counting = 0
-
 for char in input_string:
     if char.isnumeric():
         num_counting += 1
@@ -14,7 +12,6 @@ for char in input_string:
         low_counting += 1
     else:
         other_counting += 1
-
 print("numbers:", num_counting)
 print("uppercase letters:", upp_counting)
 print("lowercase letters:", low_counting)
